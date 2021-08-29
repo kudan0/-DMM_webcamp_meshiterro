fd0d97210eb2408296785014c87e2779c698c680
kudan0 <7xxx.ccs@gmail.com>
2021-08-29 14:45:41 +0000

    [finish]3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'f00332bf8180a24464fe904dad1b8d6f1b584b320e85da0f9a9419d6c7fb260b36c469e1785570d658034b3f616ea33b5c57566bc2d5f552a703a7ee675aed6a'
